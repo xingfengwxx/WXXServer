fb620c2a339970683f93b83663966cf200d5b531
WangXingxing <1099420259@qq.com>
2026-01-09 11:58:48 +0800

    feat: 初始化 Spring Boot 项目

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.netflix.dgs.codegen") version "8.3.0"
+    id("org.graalvm.buildtools.native") version "0.11.3"
+}
+
+group = "com.wangxingxing.wxxserver"
+version = "0.0.1-SNAPSHOT"
+description = "WXXServer"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springModulithVersion"] = "2.0.1"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.generateJava {
+    schemaPaths.add("${projectDir}/src/main/resources/graphql-client")
+    packageName = "com.wangxingxing.wxxserver.wxxserver.codegen"
+    generateClient = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/wangxingxing/wxxserver/wxxserver/WxxServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wangxingxing.wxxserver.wxxserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WxxServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
